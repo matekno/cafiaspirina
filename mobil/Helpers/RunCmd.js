212ed831f41cd68b16d009350ffe60c634a1882b
matekno <mati.rapoport@gmail.com>
2022-08-14 18:14:25 -0300

    conexion con mobile

--- mobil/Helpers/RunCmd.js
+++ mobil/Helpers/RunCmd.js
@@ -4,11 +4,11 @@ import base64 from 'base-64'
 const url = "http://192.168.56.1:3000"
 
 const ExecInServer = async (endpoint, username, password) => {
-
-
+    const authString = `Basic ${base64.encode(username + ":" + password)}`
+    console.log(authString);
     const data = await axios.get(`${url}/${endpoint}`, {
         headers: {
-            Authorization: "Basic " + base64.encode(username + password),
+            Authorization: authString,
         }
     });
     console.log(data);
