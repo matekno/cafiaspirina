d0c7986d506392345a660823957699222561d81c
matekno <mati.rapoport@gmail.com>
2022-08-14 22:35:50 -0300

    add auth rn

--- mobil/App.js
+++ mobil/App.js
@@ -1,15 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import ButtonExecCmd from './components/GetCmd';
 import cmds from './cmd.json'; // no anda el cmd.json gral
 
 export default function App() {
+  const [user, onChangeUser] = useState('');
+  const [pwd, onChangePwd] = useState('');
+
   return (
     <View style={styles.container}>
+      <TextInput
+        onChangeText={onChangeUser}
+        value={user}
+        placeholder="Usuario" />
+      <TextInput
+        onChangeText={onChangePwd}
+        secureTextEntry={true}
+        value={pwd}
+        placeholder="Password" />
       {
         cmds.commands.map((cmd) => {
           return (
-            <ButtonExecCmd cmd={cmd.endpoint} username="admin" password="1234" title={cmd.endpoint} />
+            <ButtonExecCmd cmd={cmd.endpoint} username={user} password={pwd} title={cmd.endpoint} />
           )
         })
       }
